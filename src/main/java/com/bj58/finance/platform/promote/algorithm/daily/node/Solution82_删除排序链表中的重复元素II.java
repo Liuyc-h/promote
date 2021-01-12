2ca9c8466be405f26d6eb5169a1031e50e0417c5
liuyongchao02 <liuyongchao02@58.com>
2021-01-12 14:42:10 +0800

    '2020-01-12'

--- src/main/java/com/bj58/finance/platform/promote/algorithm/daily/node/Solution82_删除排序链表中的重复元素II.java
+++ src/main/java/com/bj58/finance/platform/promote/algorithm/daily/node/Solution82_删除排序链表中的重复元素II.java
@@ -5,7 +5,7 @@ import com.bj58.finance.platform.promote.algorithm.struct.ListNode;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
+/***
  *  给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
  *
  * 示例 1:
@@ -20,49 +20,47 @@ import java.util.Set;
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- * **/
-public class Solution82 {
+ *
+ * ***/
+public class Solution82_删除排序链表中的重复元素II {
     /**
-     *  先遍历一遍链表，查询出重复的值，放在Set集合里面，
-     *  然后在遍历一次链表，找出重复的节点删除
-     *  需要遍历两次链表
+     *  先存储下重复元素，然后删除，需遍历两遍
      * **/
     public ListNode deleteDuplicates(ListNode head) {
+        //特殊情况处理
         if(head == null || head.next == null){
             return head;
         }
-        Set<Integer> intSet = new HashSet<>();
-        ListNode queryRepeatNode = head.next;
-        int startVal = head.value;
-        while(queryRepeatNode != null){
-            //不相等的话流转一下
-            if(startVal != queryRepeatNode.value){
-                startVal = queryRepeatNode.value;
-                queryRepeatNode = queryRepeatNode.next;
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> repeatSet = new HashSet<>();
+        ListNode flowNode = head;
+        while(flowNode != null){
+            if(set.contains(flowNode.value)){
+                repeatSet.add(flowNode.value);
             }else{
-                intSet.add(queryRepeatNode.value);
-                queryRepeatNode = queryRepeatNode.next;
+                set.add(flowNode.value);
             }
+            flowNode = flowNode.next;
         }
         ListNode resultNode = new ListNode(0);
         ListNode resultFlowNode = resultNode;
-        ListNode flowNode = head;
-        while(flowNode != null){
-            int val = flowNode.value;
-            if(!intSet.contains(val)){
-                resultFlowNode.next = flowNode;
-                flowNode = flowNode.next;
+
+        while(head != null){
+            if(!repeatSet.contains(head.value)){
+                ListNode node = head;
+                head = head.next;
+                resultFlowNode.next = node;
                 resultFlowNode = resultFlowNode.next;
                 resultFlowNode.next = null;
             }else{
-                flowNode = flowNode.next;
+                head = head.next;
             }
         }
         return resultNode.next;
     }
 
     /**
-     *  记录节点和节点值的出现频率，
+     *  记录出现的顺序
      * **/
     public ListNode deleteDuplicates1(ListNode head) {
         if(head == null || head.next == null){
@@ -106,11 +104,9 @@ public class Solution82 {
         return resultNode.next;
     }
 
-
     public static void main(String[] args) {
-        ListNode node = ListNode.initListNode(new int[]{1,2,2});
+        ListNode node = ListNode.initListNode(new int[]{1,1,1,2,3});
 
-
-        System.out.println(new Solution82().deleteDuplicates1(node));
+        System.out.println(new Solution82_删除排序链表中的重复元素II().deleteDuplicates(node));
     }
 }
