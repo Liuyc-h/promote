5ac4742a6912b26a719f2b1ee7e5acdc7797913e
liuyongchao02 <liuyongchao02@58.com>
2021-01-21 19:15:58 +0800

    ''

--- src/main/java/com/bj58/finance/platform/promote/algorithm/daily/String/Solution925_长按键入.java
+++ src/main/java/com/bj58/finance/platform/promote/algorithm/daily/String/Solution925_长按键入.java
@@ -1,7 +1,7 @@
 package com.bj58.finance.platform.promote.algorithm.daily.String;
 
 /**
- *你的朋友正在使用键盘输入他的名字 name。偶尔，在键入字符 c 时，按键可能会被长按，而字符可能被输入 1 次或多次。
+ *  你的朋友正在使用键盘输入他的名字 name。偶尔，在键入字符 c 时，按键可能会被长按，而字符可能被输入 1 次或多次。
  *
  * 你将会检查键盘输入的字符 typed。如果它对应的可能是你的朋友的名字（其中一些字符可能被长按），那么就返回 True。
  *
@@ -26,46 +26,44 @@ package com.bj58.finance.platform.promote.algorithm.daily.String;
  * 输入：name = "laiden", typed = "laiden"
  * 输出：true
  * 解释：长按名字中的字符并不是必要的。
+ *  
+ *
+ * 提示：
+ *
+ * name.length <= 1000
+ * typed.length <= 1000
+ * name 和 typed 的字符都是小写字母。
  *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/long-pressed-name
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
- *
- * */
-public class Solution925 {
+ * **/
+public class Solution925_长按键入 {
 
     public boolean isLongPressedName(String name, String typed) {
 
-        if(name == null || typed == null || name.length() > typed.length()){
+        if(name == null || typed == null ){
             return false;
         }
-        int i= 0,j = 0;
-        while( j < typed.length() ){
+        if(name.equals(typed)){
+            return true;
+        }
 
-            if(i < name.length() && name.charAt(i) == typed.charAt(j)){
-                i++;
-                j++;
+        int i= 0,j = 0;
+        while(j < typed.length()){
+             //如果name.charAt(i) == typed.charAt(j) 同时相加，如果不想等了，
+            //看看typed是否有重复的，如果没有，就是失败了
+            //遍历完看看长度，是否都一样
+            if (i < name.length() && name.charAt(i) == typed.charAt(j)){
+                i ++;
+                j ++;
             }else if(j > 0 && typed.charAt(j) == typed.charAt(j - 1)){
-                j++;
+                j ++;
             }else{
                 return false;
             }
-
         }
         return i == name.length();
     }
-
-    public static void main(String[] args) {
-
-        String name = "vtkgn";
-
-        String typed = "vtkgnn";
-
-        System.out.println(new Solution925().isLongPressedName(name,typed));
-    }
-
-
-
-
 }
