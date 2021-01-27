efbae8e5a6369e85d672815bd62e4bee189ffd3f
liuyongchao02 <liuyongchao02@58.com>
2021-01-27 11:06:55 +0800

    ''

--- src/main/java/com/bj58/finance/platform/promote/tuling/spring/ApplicationContextExample.java
+++ src/main/java/com/bj58/finance/platform/promote/tuling/spring/ApplicationContextExample.java
@@ -3,6 +3,9 @@ package com.bj58.finance.platform.promote.tuling.spring;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
@@ -16,6 +19,8 @@ public class ApplicationContextExample {
         ApplicationContext context = new ClassPathXmlApplicationContext();
 
         ApplicationContext context1 = new AnnotationConfigApplicationContext();
+
+//        InstantiationAwareBeanPostProcessor
 
 //        BeanFactory
 //        FactoryBean
